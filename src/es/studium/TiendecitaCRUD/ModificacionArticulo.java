530a599a718ceba7cf4c75684ca963b925bb7baa
Nazaret Laguna <lagunanazaret@gmail.com>
2021-06-16 22:26:39 +0200

    Clase ModificacionArticulo incapaz de conectar a la DB

--- src/es/studium/TiendecitaCRUD/ModificacionArticulo.java
+++ src/es/studium/TiendecitaCRUD/ModificacionArticulo.java
@@ -1,215 +1,253 @@
 package es.studium.TiendecitaCRUD;
 
+import java.awt.BorderLayout;
 import java.awt.EventQueue;
-import java.awt.Font;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
-import javax.swing.border.EmptyBorder;
-import javax.swing.JPopupMenu;
 
-import java.awt.Choice;
-import java.awt.Component;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+
+
+import javax.swing.JLabel;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-
-import javax.swing.JComboBox;
-
+import java.awt.Choice;
+import javax.swing.JTextField;
+import javax.swing.JButton;
 
 public class ModificacionArticulo extends JFrame
 {
 
-
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 	private JTextField textField;
 	private JTextField textField_1;
 	private JTextField textField_2;
-
 	Choice choice = new Choice();
-	public static void main(String[] args)
+	private JButton btnNewButton;
+	private JButton btnNewButton_1;
+	private JLabel lblNewLabel_4;
+	private JTextField textField_3;
+	/**
+	 * Launch the application.
+	 */
+/*	public static void main(String[] args)
 	{
 		EventQueue.invokeLater(new Runnable()
 		{
 			public void run()
 			{
-				try
-				{
+				try {
 					ModificacionArticulo frame = new ModificacionArticulo();
 					frame.setVisible(true);
-				} catch (Exception e)
-				{
+				} catch (Exception e) {
 					e.printStackTrace();
 				}
 			}
 		});
-		
-			
-		}
+	}*/
+
 	/**
 	 * Create the frame.
 	 */
 	public ModificacionArticulo()
 	{
-		setTitle("Tiendecita - Artículos");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
-		JLabel lblArtculos = new JLabel("Modificación");
-		lblArtculos.setFont(new Font("Tahoma", Font.PLAIN, 17));
-		lblArtculos.setBounds(170, 13, 97, 30);
-		contentPane.add(lblArtculos);
-		
-		JButton btnNewButton = new JButton("Modificar");
-		btnNewButton.setBounds(164, 198, 103, 30);
+
+		JLabel lblNewLabel = new JLabel("Modificaci\u00F3n");
+		lblNewLabel.setFont(new Font("Arial", Font.PLAIN, 14));
+		lblNewLabel.setBounds(138, 11, 87, 30);
+		contentPane.add(lblNewLabel);
+
+		JLabel lblNewLabel_1 = new JLabel("Descripci\u00F3n");
+		lblNewLabel_1.setBounds(46, 156, 87, 14);
+		contentPane.add(lblNewLabel_1);
+
+		JLabel lblNewLabel_2 = new JLabel("Precio");
+		lblNewLabel_2.setBounds(46, 181, 46, 14);
+		contentPane.add(lblNewLabel_2);
+
+		JLabel lblNewLabel_3 = new JLabel("Cantidad");
+		lblNewLabel_3.setBounds(43, 206, 66, 14);
+		contentPane.add(lblNewLabel_3);
+
+		lblNewLabel_4 = new JLabel("ID");
+		lblNewLabel_4.setBounds(46, 118, 46, 14);
+		contentPane.add(lblNewLabel_4);
+
+		textField = new JTextField(); //Descripcion
+		textField.setBounds(151, 152, 140, 20);
+		contentPane.add(textField);
+		textField.setColumns(10);
+
+
+		textField_1 = new JTextField();
+		textField_1.setBounds(151, 183, 140, 20);
+		contentPane.add(textField_1);
+		textField_1.setColumns(10);
+
+		textField_2 = new JTextField();
+		textField_2.setBounds(151, 214, 140, 20);
+		contentPane.add(textField_2);
+		textField_2.setColumns(10);
+
+		textField_3 = new JTextField(); //ID
+		textField_3.setBounds(151, 115, 140, 20);
+		contentPane.add(textField_3);
+		textField_3.setColumns(10);
+
+		choice.setBounds(120, 62, 158, 20);
+		contentPane.add(choice);
+
+		btnNewButton = new JButton("Modificar");
+		btnNewButton.setBounds(317, 157, 89, 23);
 		contentPane.add(btnNewButton);
-		
-		JButton btnSalir = new JButton("Volver");
-		btnSalir.setBounds(323, 215, 97, 25);
-		contentPane.add(btnSalir);
-		btnSalir.addActionListener(new ActionListener() {
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+
+				ConfirmacionModificacion.main(null);
+
+				//				Connection con = conectar();
+				//			//	modificar(con, textField, textField_1, textField_2);
+				//				
+				//				int id = Integer.parseInt(textField_3.getText());
+				//				String descripcion = textField.getText();
+				//				int precio = Integer.parseInt(textField_1.getText());
+				//				int cantidad = Integer.parseInt(textField_2.getText());
+				//				
+				//				// Ejecutar el UPDATE
+				//				String sqlUpdate = "UPDATE articulos SET descripcionArticulo = '" + descripcion 
+				//						+ "', precioArticulo = '" 
+				//						+ precio + "', cantidadArticulo = '" + cantidad
+				//						+ "'  WHERE idArticulo = '" + id + "'";
+				//				System.out.println(sqlUpdate);
+				//				setVisible(false);
+				//				//		frmEditar.setVisible(true);
+				//				try
+				//				{
+				//					// CREAR UN STATEMENT PARA UNA CONSULTA SELECT
+				//					Statement stmt = con.createStatement();
+				//					stmt.executeUpdate(sqlUpdate);
+				//					System.out.println("Actualizacion completada correctamente...");
+				//					stmt.close();
+				//				} catch (SQLException ex)
+				//				{
+				//					System.out.println("ERROR:al modificar");
+				//					ex.printStackTrace();
+				//				}
+				//
+			}
+
+
+		});
+
+		btnNewButton_1 = new JButton("Volver");
+		btnNewButton_1.setBounds(317, 213, 89, 23);
+		contentPane.add(btnNewButton_1);
+
+		btnNewButton_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 
 				setVisible(false);
-				new Articulos();
+
 			}
 		});
-		
-		textField = new JTextField("");  //descripcion
-		textField.setBounds(116, 83, 192, 22);
-		contentPane.add(textField);
-		textField.setColumns(10);
-		choice.setBounds(114, 45, 217, 22);
-		contentPane.add(choice);
-		textField.setText(choice.getSelectedItem());
-		choice.addItemListener(new ItemListener(){
-			  public void itemStateChanged(ItemEvent ie){
 
-			    textField.setText(choice.getSelectedItem());
-			  }
-			});
-		
-		JLabel lblNewLabel = new JLabel("Descripci\u00F3n");
-		lblNewLabel.setBounds(32, 86, 79, 16);
-		contentPane.add(lblNewLabel);
-		
-		JLabel lblPrecio = new JLabel("Precio");
-		lblPrecio.setBounds(32, 115, 79, 16);
-		contentPane.add(lblPrecio);
-		
-		textField_1 = new JTextField("");  //  precio
-		textField_1.setColumns(10);
-		textField_1.setBounds(116, 112, 192, 22);
-		contentPane.add(textField_1);
-		textField_1.setText(choice.getSelectedItem());
-		choice.addItemListener(new ItemListener(){
-			  public void itemStateChanged(ItemEvent ie){
 
-			    textField_1.setText(choice.getSelectedItem());
-			  }
-			});
-		
-		JLabel lblCantidad = new JLabel("Cantidad");
-		lblCantidad.setBounds(32, 147, 79, 16);
-		contentPane.add(lblCantidad);
-		
-		textField_2 = new JTextField(104);  //cantidad
-		textField_2.setColumns(10);
-		textField_2.setBounds(116, 144, 192, 22);
-		contentPane.add(textField_2);
-		textField_2.setText(choice.getSelectedItem());
-		choice.addItemListener(new ItemListener(){
-			  public void itemStateChanged(ItemEvent ie){
+		choice.add("Selecciona un artículo");
 
-			    textField_2.setText(choice.getSelectedItem());
-			  }
-			});
-		
-		// un desplegable en el que el usuario podrá escribir para buscar en el CRUD
-	/*	JComboBox comboBox = new JComboBox();
-		comboBox.setEditable(true);
-		comboBox.setBounds(116, 45, 192, 22);
-		contentPane.add(comboBox);
-//		comboBox.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Buscar artículo ...", "Folios dinA4", "Carpetas 002 aut", "Lápiz stabilo 202" }));
-	*/	
-		
-	
-		Connection conexion=conectar(); //Conexión para extraer los inserts o datos dentro de cada fila de la DB
-		Statement stmt;
+		choice.addItemListener(new ItemListener(){
+			public void itemStateChanged(ItemEvent ie){
+				Object fuente = ie.getItem();
+				if(fuente.equals(choice.getSelectedItem())) {
+
+					Connection conect = conectar();
+					// Sacar el id del elemento elegido
+					int id = Integer.parseInt(choice.getSelectedItem().split("-")[0]);
+					mostrarDatos(conect, id);	
+					// textField.setText(choice.getSelectedItem());
+
+
+
+				}else {
+					textField.setText("");
+				}
+
+			}
+		});
+
+		// Conectar a la base de datos
+		Connection con = conectar();
+		// Sacar los datos de la tabla edificios
+		// Rellenar el Choice
+		String sqlSelect = "SELECT * FROM articulos";
 		try
 		{
-			stmt = conexion.createStatement();	//query para los inserts 
-			String query = "SELECT * FROM articulos"; 
-			ResultSet rs = stmt.executeQuery(query);
-			
-			while(rs.next())  // mientras tengamos registros en el resultset
+			// CREAR UN STATEMENT PARA UNA CONSULTA SELECT
+			Statement stmt = con.createStatement();
+			ResultSet rs = stmt.executeQuery(sqlSelect);
+			while (rs.next())
 			{
-				choice.add(rs.getInt("idArticulo")+"-"+rs.getString("descripcionArticulo")+","+rs.getInt("cantidadArticulo")+","+rs.getInt("precioArticulo"));
+				choice.add(rs.getInt("idArticulo") + "-" + rs.getString("descripcionArticulo") + ", "
+						+ rs.getInt("precioArticulo") + "-" + rs.getInt("cantidadArticulo"));
 			}
-			
-			stmt.close(); //cerramos los recursos y la conexion
-			conexion.close(); //
-	//		System.out.println("El alta ha sido realizada correctamente.");
-		
-		} catch (SQLException e1)
+			rs.close();
+			stmt.close();
+			con.close();
+		} catch (SQLException ex)
 		{
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}	
-		
-		setVisible(true);
+			System.out.println("ERROR:al consultar");
+			ex.printStackTrace();
+		}
+
+
+
 	}
-/*	private static void addPopup(Component component, final JPopupMenu popup) {
-		component.addMouseListener(new MouseAdapter() {
-			public void mousePressed(MouseEvent e) {
-				if (e.isPopupTrigger()) {
-					showMenu(e);
-				}
-			}
-			public void mouseReleased(MouseEvent e) {
-				if (e.isPopupTrigger()) {
-					showMenu(e);
-				}
-			}
-			private void showMenu(MouseEvent e) {
-				popup.show(e.getComponent(), e.getX(), e.getY());
-			}
-		});
-	}*/
+
+
+
+
 	public Connection conectar()
 	{
 		String DB_URL = "jdbc:mysql://localhost:3306/tiendecita?serverTimezone=GMT"; //conexion con mysql
 		String DB_USER = "root";  // root y pass necesarios para acceder a datos (luego entraremos con un admin o usuario ya creado)
-		String DB_PASS = "grupostudium";
+		String DB_PASS = "grupostudium;";
 
 		Connection conexion = null;
-//		try
-//		{
-//			Class.forName("com.mysql.jdbc.Driver");
-//		} catch (ClassNotFoundException e)
-//		{
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
+		//	try
+		//{
+		//		Class.forName("com.mysql.cj.jdbc.Driver");
+		//		} catch (ClassNotFoundException e)
+		{
+			// TODO Auto-generated catch block
+
+
+
+			//e.printStackTrace();
+		}
 		try
 		{
 			conexion = DriverManager.getConnection(DB_URL, DB_USER, DB_PASS);
 			System.out.println("La conexión se ha realizado correctamente.");
-			
+
 		} catch (SQLException e)
 		{
 			// TODO Auto-generated catch block
@@ -218,4 +256,73 @@ public class ModificacionArticulo extends JFrame
 		}
 		return conexion;
 	}
+
+	public void mostrarDatos(Connection con, int id)
+	{
+		String sqlSelect = "SELECT * FROM articulos WHERE idArticulo = " + id;
+		try
+		{
+
+			// Creamos un STATEMENT para una consulta SQL INSERT.
+			Statement sta = con.createStatement();
+			ResultSet rs = sta.executeQuery(sqlSelect);
+			while (rs.next())
+			{
+				String idTxt = rs.getString("idArticulo");
+				textField_3.setText((idTxt));				
+				String descripcion = rs.getString("descripcionArticulo");
+				textField.setText((descripcion));
+				String precio = rs.getString("precioArticulo");
+				textField_1.setText((precio));
+				String cantidad = rs.getString("cantidadArticulo");
+				textField_2.setText((cantidad));
+
+			}
+			sta.close();
+			con.close();
+		} catch (SQLException ex)
+		{
+			System.out.println("ERROR:al hacer un Delete");
+			ex.printStackTrace();
+		}
+
+
+		try {
+
+			setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+			setVisible(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+	//	public void modificar(Connection con,JTextField campo_id ,JTextField campo_2,JTextField campo_3,JTextField campo_4) {
+	//
+	//		int id = Integer.parseInt(campo_id.getText());
+	//		String descripcion = campo_2.getText();
+	//		int precio = Integer.parseInt(campo_3.getText());
+	//		int cantidad = Integer.parseInt(campo_4.getText());
+	//
+	//		// Ejecutar el UPDATE
+	//		String sqlUpdate = "UPDATE articulos SET descripcionArticulo = '" + descripcion 
+	//				+ "', precioArticulo = '" 
+	//				+ precio + "', cantidadArticulo = '" + cantidad
+	//				+ "'  WHERE idArticulo = '" + id + "'";
+	//		System.out.println(sqlUpdate);
+	//		setVisible(false);
+	//		//		frmEditar.setVisible(true);
+	//		try
+	//		{
+	//			// CREAR UN STATEMENT PARA UNA CONSULTA SELECT
+	//			Statement stmt = con.createStatement();
+	//			stmt.executeUpdate(sqlUpdate);
+	//			System.out.println("Actualizacion completada correctamente...");
+	//			stmt.close();
+	//		} catch (SQLException ex)
+	//		{
+	//			System.out.println("ERROR:al modificar");
+	//			ex.printStackTrace();
+	//		}
+	//
+	//	}
 }
